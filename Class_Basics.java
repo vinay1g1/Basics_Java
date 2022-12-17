5b4004c769c89b19951f1bb1b74daf8a97a2bb2f
vinay1g1 <vinaygiri232323@gmail.com>
2022-12-17 03:19:06 -0800

    created basics java variable

---
+++ Class_Basics.java
@@ -0,0 +1,9 @@
+package basicsofjava;
+
+public class Class_Basics {
+	
+	public static void main(String[] args) {
+		System.out.println("Hello world");
+	}
+
+}
---
+++ Variable.java
@@ -0,0 +1,39 @@
+package basicsofjava;
+
+public class Variable {
+
+	public static void main(String[] args) {
+		/* variable is a named memory location which can store some value/data and it can change
+		n number of times */
+
+		/* byte/short/int/long/float/double/char/boolean */
+		
+		int a=10;
+		System.out.println(a);
+		
+		double emp_sal=65540.76;
+		System.out.println(emp_sal);
+		
+		char grade = 'A';
+		System.out.println(grade);
+		
+		String name = "vinay";
+		System.out.println(name);
+		
+		float price = 10.08f;
+		System.out.println(price);
+		
+		boolean isit = true;
+		boolean orisit = false;
+		System.out.println(isit);
+		System.out.println(orisit);
+		
+		byte val = 0101;
+		System.out.println(val);
+		
+		short val2 = 123; 
+		System.out.println(val2);
+		
+	}
+
+}
