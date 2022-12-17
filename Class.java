150e6a4ed63f7b781dabb170b91ea61c59dc6abb
vinay1g1 <vinaygiri232323@gmail.com>
2022-12-16 23:08:46 -0800

    created java basic class

---
+++ Class.java
@@ -0,0 +1,11 @@
+package All_Basics;
+
+public class Class {
+//class is a blueprint or template to create an object.
+	public static void main(String[] args) {
+		
+		System.out.println("Hello world");
+
+	}
+
+}
